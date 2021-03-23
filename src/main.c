978e8f0ac8ab494d2aae42ad67ce059cbbf1ac40
mannie.exe <root@unknown.energy>
2021-03-22 18:52:27 -0700

    Split main

--- src/main.c
+++ src/main.c
@@ -1,16 +1,34 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <GLFW/glfw3.h>
 #include <glad/gl.h>
+
+const int WIDTH = 1920;
+const int HEIGHT = 1080;
+int glVersion = 0;
+
+GLFWwindow *mainWindow = NULL;
 
 void key_callback(GLFWwindow *window,
                   int key, int scancode,
                   int action, int mode)
 {
+    /**
+     * action:
+     *  0: release
+     *  1: down
+     *  2: hold
+     **/
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-int main()
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void init_main()
 {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -24,36 +42,52 @@ int main()
      *
     **/
 
-    GLFWwindow *window = glfwCreateWindow(1920, 1080, "GLFW -> GLAD (OpenGL in C)", NULL, NULL);
-    if (window == NULL)
+    mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "GLFW -> GLAD (OpenGL in C)", NULL, NULL);
+    if (mainWindow == NULL)
     {
         printf("Failed to create window using GLFW\n");
         glfwTerminate();
-        return -1;
+        abort();
     }
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(mainWindow);
 
-    int version = gladLoadGL(glfwGetProcAddress);
-    if (version == 0)
+    glVersion = gladLoadGL(glfwGetProcAddress);
+    if (glVersion == 0)
     {
         printf("Failed to get OpenGL context\n");
         glfwTerminate();
-        return -1;
+        abort();
     }
     printf("Loaded OpenGL v%d.%d\n",
-           GLAD_VERSION_MAJOR(version),
-           GLAD_VERSION_MINOR(version));
+           GLAD_VERSION_MAJOR(glVersion),
+           GLAD_VERSION_MINOR(glVersion));
 
-    while (!glfwWindowShouldClose(window))
+    glfwSetKeyCallback(mainWindow, key_callback);
+    glfwSetFramebufferSizeCallback(mainWindow, framebuffer_size_callback);
+}
+
+void close_main()
+{
+    glfwTerminate();
+}
+
+void draw_main()
+{
+    while (!glfwWindowShouldClose(mainWindow))
     {
         glfwPollEvents();
         glClearColor(0.27f, 0.87f, 0.75f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(mainWindow);
     }
+}
 
-    glfwTerminate();
+int main()
+{
+    init_main();
+    draw_main();
+
+    close_main();
     return 0;
 }
