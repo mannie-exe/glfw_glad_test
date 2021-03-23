73c37b467aeade00f9d714d75a0d0df54f9d226d
mannie.exe <root@unknown.energy>
2021-03-22 18:17:46 -0700

    Initialize GLFW and GLAD

--- src/main.c
+++ src/main.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include <GLFW/glfw3.h>
+#include <glad/gl.h>
+
+void key_callback(GLFWwindow *window,
+                  int key, int scancode,
+                  int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main()
 {
-    printf("hello world!");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    /**
+     * if (mac)
+     * {
+     *      glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+     * }
+     *
+    **/
+
+    GLFWwindow *window = glfwCreateWindow(1920, 1080, "GLFW -> GLAD (OpenGL in C)", NULL, NULL);
+    if (window == NULL)
+    {
+        printf("Failed to create window using GLFW\n");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0)
+    {
+        printf("Failed to get OpenGL context\n");
+        glfwTerminate();
+        return -1;
+    }
+    printf("Loaded OpenGL v%d.%d\n",
+           GLAD_VERSION_MAJOR(version),
+           GLAD_VERSION_MINOR(version));
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClearColor(0.27f, 0.87f, 0.75f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
