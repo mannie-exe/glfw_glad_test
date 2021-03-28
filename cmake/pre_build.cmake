faac8644b5eefe678283ae3895694374bbcab8c0
mannie.exe <root@unknown.energy>
2021-03-28 13:01:39 -0700

    Implement image bundling, but disable 'til needed

--- cmake/pre_build.cmake
+++ cmake/pre_build.cmake
@@ -27,3 +27,30 @@ foreach(_shader ${${PROJECT_NAME}_GLSL_FILES})
     "Binary <- GLSL (xxd): include/${_shader_NAME}.h <- ${_shader_XXD_INPUT}"
   )
 endforeach()
+
+# foreach(_image ${${PROJECT_NAME}_IMAGE_FILES})
+#   cmake_path(GET _image STEM LAST_ONLY _image_NAME)
+#   cmake_path(GET _image EXTENSION LAST_ONLY _image_EXT)
+#   string(REPLACE "." "" _image_EXT ${_image_EXT})
+
+#   cmake_path(GET _image PARENT_PATH _image_XXD_WDIR)
+#   cmake_path(GET _image_XXD_WDIR PARENT_PATH _image_XXD_WDIR)
+
+#   cmake_path(SET _image_XXD_INPUT
+#              "images/${_image_NAME}.${_image_EXT}")
+#   cmake_path(SET _image_XXD_OUTPUT
+#              "${PROJECT_SOURCE_DIR}/include/images_${_image_NAME}_${_image_EXT}.h")
+
+#   message(STATUS "${_image_XXD_INPUT} <- ${_image_XXD_OUTPUT}")
+
+#   add_custom_command(
+#     TARGET ${PROJECT_NAME} PRE_BUILD
+#     COMMAND ${XXD} -i
+#     ${_image_XXD_INPUT}
+#     ${_image_XXD_OUTPUT}
+#     WORKING_DIRECTORY
+#     ${_image_XXD_WDIR}
+#     COMMENT
+#     "Binary <- Image (xxd): include/${_image_NAME}.h <- ${_image_XXD_INPUT}"
+#   )
+# endforeach()
