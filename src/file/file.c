e4c4788add476b54342230a7abe3ee988812cf52
mannie.exe <root@unknown.energy>
2021-03-28 12:25:00 -0700

    Restructure main; delete file module
    * use pre-processor definitions for constants
    * initialize functions at the top
    * delete file module, will implement later

--- src/file/file.c
+++
@@ -1,6 +0,0 @@
-#include <file.h>
-
-const char *read_file(const char *file_path)
-{
-    return file_path;
-}
--- src/main.c
+++ src/main.c
@@ -9,35 +9,40 @@
 #include "GLFW/glfw3.h"
 #include "glad/gl.h"
 
-#include "file.h"
+#define INITIAL_WIDTH 1920
+#define INITIAL_HEIGHT 1080
 
-int glVersion = 0;
-
-const int INITIAL_WIDTH = 1920;
-const int INITIAL_HEIGHT = 1080;
 GLFWwindow *mainWindow = NULL;
-
+int glVersion;
 unsigned int ebo_triangle;
 unsigned int shaderProgram_triangle;
 
+void init_main();
+void draw_main();
+void close_main();
 void callback_key(GLFWwindow *window,
                   int key, int scancode,
-                  int action, int mode)
-{
-    /**
-     * action:
-     *  0: release  GLFW_RELEASE
-     *  1: press    GLFW_PRESS
-     *  2: hold     GLFW_REPEAT
-     **/
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
+                  int action, int mode);
 void callback_framebufferSize(GLFWwindow *window,
-                              int width, int height)
+                              int width, int height);
+void init_triangle();
+unsigned int init_gl_shaderProgram(const char *gl_vertexSource,
+                                   const char *gl_fragmentSource);
+unsigned int compile_gl_shader(unsigned int gl_vertexShader,
+                               unsigned int gl_fragmentShader);
+unsigned int load_gl_shader(const char *gl_shaderSource,
+                            int gl_shaderType);
+void abort_gl_shaderError(unsigned int *gl_shaderIndex);
+
+int main()
 {
-    glViewport(0, 0, width, height);
+    init_main();
+    init_triangle();
+
+    draw_main();
+
+    close_main();
+    return 0;
 }
 
 void init_main()
@@ -87,96 +92,35 @@ void draw_main()
     while (!glfwWindowShouldClose(mainWindow))
     {
         glfwPollEvents();
-        // glClearColor(0.27f, 0.87f, 0.75f, 1.0f);
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
         glUseProgram(shaderProgram_triangle);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_triangle);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-        // glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
         glfwSwapBuffers(mainWindow);
     }
 }
 
-void abort_gl_shaderError(unsigned int *gl_shaderIndex)
+void callback_key(GLFWwindow *window,
+                  int key, int scancode,
+                  int action, int mode)
 {
-    char infoLog[512];
-    glGetShaderInfoLog(*gl_shaderIndex, 512, NULL, infoLog);
-
-    printf("Failed to compile shader | GL_ERROR:\n%s\n", infoLog);
-
-    close_main();
-    abort();
+    /**
+     * action:
+     *  0: release  GLFW_RELEASE
+     *  1: press    GLFW_PRESS
+     *  2: hold     GLFW_REPEAT
+     **/
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-unsigned int load_gl_shader(const char *gl_shaderSource,
-                            int gl_shaderType)
+void callback_framebufferSize(GLFWwindow *window,
+                              int width, int height)
 {
-    int gl_shaderCompileSuccess;
-    unsigned int gl_shaderIndex;
-
-    gl_shaderIndex = glCreateShader(gl_shaderType);
-    glShaderSource(gl_shaderIndex, 1,
-                   &gl_shaderSource, NULL);
-    glCompileShader(gl_shaderIndex);
-
-    glGetShaderiv(gl_shaderIndex, GL_COMPILE_STATUS,
-                  &gl_shaderCompileSuccess);
-    if (!gl_shaderCompileSuccess)
-        abort_gl_shaderError(&gl_shaderIndex);
-
-    return gl_shaderIndex;
-}
-
-unsigned int compile_gl_shader(unsigned int gl_vertexShader,
-                               unsigned int gl_fragmentShader)
-{
-    int gl_shaderLinkSuccess;
-    unsigned int gl_shaderProgramIndex;
-
-    gl_shaderProgramIndex = glCreateProgram();
-    glAttachShader(gl_shaderProgramIndex,
-                   gl_vertexShader);
-    glAttachShader(gl_shaderProgramIndex,
-                   gl_fragmentShader);
-    glLinkProgram(gl_shaderProgramIndex);
-
-    glGetProgramiv(gl_shaderProgramIndex, GL_LINK_STATUS,
-                   &gl_shaderLinkSuccess);
-    if (!gl_shaderLinkSuccess)
-    {
-        char infoLog[512];
-        glGetShaderInfoLog(gl_shaderProgramIndex, 512, NULL, infoLog);
-        printf("Failed to link shader program | GL_ERROR:\n%s\n", infoLog);
-        close_main();
-        abort();
-    }
-
-    return gl_shaderProgramIndex;
-}
-
-unsigned int init_gl_shaderProgram(const char *gl_vertexSource,
-                                   const char *gl_fragmentSource)
-{
-    unsigned int gl_vertexShader;
-    unsigned int gl_fragmentShader;
-    unsigned int gl_shaderProgram;
-
-    gl_vertexShader = load_gl_shader(gl_vertexSource,
-                                     GL_VERTEX_SHADER);
-    gl_fragmentShader = load_gl_shader(gl_fragmentSource,
-                                       GL_FRAGMENT_SHADER);
-    gl_shaderProgram = compile_gl_shader(gl_vertexShader,
-                                         gl_fragmentShader);
-
-    glDeleteShader(gl_vertexShader);
-    glDeleteShader(gl_fragmentShader);
-
-    return gl_shaderProgram;
+    glViewport(0, 0, width, height);
 }
 
 void init_triangle()
@@ -248,14 +192,79 @@ void init_triangle()
     shaderProgram_triangle = gl_shaderProgramIndex;
 }
 
-int main()
+unsigned int init_gl_shaderProgram(const char *gl_vertexSource,
+                                   const char *gl_fragmentSource)
 {
-    printf(read_file("file_path\n"));
-    init_main();
-    init_triangle();
+    unsigned int gl_vertexShader;
+    unsigned int gl_fragmentShader;
+    unsigned int gl_shaderProgram;
 
-    draw_main();
+    gl_vertexShader = load_gl_shader(gl_vertexSource,
+                                     GL_VERTEX_SHADER);
+    gl_fragmentShader = load_gl_shader(gl_fragmentSource,
+                                       GL_FRAGMENT_SHADER);
+    gl_shaderProgram = compile_gl_shader(gl_vertexShader,
+                                         gl_fragmentShader);
+
+    glDeleteShader(gl_vertexShader);
+    glDeleteShader(gl_fragmentShader);
+
+    return gl_shaderProgram;
+}
+
+unsigned int compile_gl_shader(unsigned int gl_vertexShader,
+                               unsigned int gl_fragmentShader)
+{
+    int gl_shaderLinkSuccess;
+    unsigned int gl_shaderProgramIndex;
+
+    gl_shaderProgramIndex = glCreateProgram();
+    glAttachShader(gl_shaderProgramIndex,
+                   gl_vertexShader);
+    glAttachShader(gl_shaderProgramIndex,
+                   gl_fragmentShader);
+    glLinkProgram(gl_shaderProgramIndex);
+
+    glGetProgramiv(gl_shaderProgramIndex, GL_LINK_STATUS,
+                   &gl_shaderLinkSuccess);
+    if (!gl_shaderLinkSuccess)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(gl_shaderProgramIndex, 512, NULL, infoLog);
+        printf("Failed to link shader program | GL_ERROR:\n%s\n", infoLog);
+        close_main();
+        abort();
+    }
+
+    return gl_shaderProgramIndex;
+}
+
+unsigned int load_gl_shader(const char *gl_shaderSource,
+                            int gl_shaderType)
+{
+    int gl_shaderCompileSuccess;
+    unsigned int gl_shaderIndex;
+
+    gl_shaderIndex = glCreateShader(gl_shaderType);
+    glShaderSource(gl_shaderIndex, 1,
+                   &gl_shaderSource, NULL);
+    glCompileShader(gl_shaderIndex);
+
+    glGetShaderiv(gl_shaderIndex, GL_COMPILE_STATUS,
+                  &gl_shaderCompileSuccess);
+    if (!gl_shaderCompileSuccess)
+        abort_gl_shaderError(&gl_shaderIndex);
+
+    return gl_shaderIndex;
+}
+
+void abort_gl_shaderError(unsigned int *gl_shaderIndex)
+{
+    char infoLog[512];
+    glGetShaderInfoLog(*gl_shaderIndex, 512, NULL, infoLog);
+
+    printf("Failed to compile shader | GL_ERROR:\n%s\n", infoLog);
 
     close_main();
-    return 0;
+    abort();
 }
